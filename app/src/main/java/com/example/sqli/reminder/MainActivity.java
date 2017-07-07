575e05ca2b3a949a6d406c8489e4902f4c863d27
oumayma <oumayma.mouloudi@gmail.com>
2017-07-07 10:52:08 +0100

    layout done

--- app/src/main/java/com/example/sqli/reminder/MainActivity.java
+++ app/src/main/java/com/example/sqli/reminder/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.sqli.reminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder mBuilder ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBuilder = new NotificationCompat.Builder(this);
     }
 }
---
+++ app/src/main/java/com/example/sqli/reminder/NotificationPublisher.java
@@ -0,0 +1,16 @@
+package com.example.sqli.reminder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by sqli on 07/07/2017.
+ */
+
+public class NotificationPublisher extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}

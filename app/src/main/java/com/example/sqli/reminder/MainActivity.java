8658c46f6fb7fd791a3346c5018daff16d097117
Oumayma Mouloudi <oumayma.mouloudi@gmail.com>
2017-07-10 20:45:14 +0100

    stop button

--- app/src/main/java/com/example/sqli/reminder/MainActivity.java
+++ app/src/main/java/com/example/sqli/reminder/MainActivity.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
@@ -19,12 +21,15 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button start ;
+    Button start , stop ;
     EditText hTime , mTime , jFreq;
     int seconds;
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
     Calendar calendar = Calendar.getInstance() ;
+    AlertDialog.Builder builder ;
+    PendingIntent pendingIntent;
+
 
     NotificationCompat.Builder mBuilder ;
 
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         start = (Button) findViewById(R.id.start);
         start.setOnClickListener(this);
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
         hTime = (EditText) findViewById(R.id.hTime);
         mTime = (EditText) findViewById(R.id.mTime);
         jFreq = (EditText) findViewById(R.id.jFreq);
@@ -46,7 +53,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        scheduleNotification(getNotification("tu dois prendre ton medicament aujourd'hui"), 5000);
+        if(view.getId() == R.id.start){
+            scheduleNotification(getNotification("tu dois prendre ton medicament aujourd'hui"), 5000);
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                builder = new AlertDialog.Builder(this);//,android.R.style.Theme_Holo_Light_Dialog
+            }else{
+                builder = new AlertDialog.Builder(this);
+            }
+
+            builder.setTitle("Debut du service");
+            builder.setMessage("le rappel de médicaments sera effectué " +
+                    "chaque " + jFreq.getText().toString() + " jour(s) à " + hTime.getText().toString() +
+                    "h " + mTime.getText().toString() + "min").show();
+        }
+        if(view.getId() == R.id.stop){
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                builder = new AlertDialog.Builder(this);
+            }else{
+                builder = new AlertDialog.Builder(this);
+            }
+            builder.setTitle("Arrêt du service").setMessage("Service arrêté").show();
+            if(alarmMgr != null && pendingIntent != null)
+                alarmMgr.cancel(pendingIntent);
+
+//            final AlertDialog a = builder.show();
+//            try{
+//                ((AppCompatTextView)a.findViewById(android.R.id.title)).setText("Arrêt du service");
+//            }catch (Exception e){
+//
+//            }
+
+            //stop the notications and kill the service
+            // scheduleNotification(getNotification("tu dois prendre ton medicament aujourd'hui"), 5000);
+        }
     }
 
     private void scheduleNotification(Notification notification, int delay) {
@@ -55,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent notificationIntent = new Intent(this, NotificationPublisher.class);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 //        long futureInMillis = SystemClock.elapsedRealtime() + delay;
 //        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
@@ -82,6 +122,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
              builder.setLights(Color.YELLOW,3000,3000);
              builder.setSmallIcon(R.drawable.oum);
              return builder.build();
+
     }
 
 
